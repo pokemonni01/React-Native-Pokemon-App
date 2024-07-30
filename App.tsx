b8903ae820e779e0f87f4bc53173faa3c866d738
Wachirapong Prasertwong <ni@oozou.com>
2024-07-30 16:11:10 +0700

    setup redux for dark mode toggle

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import SettingScreen from './src/screens/SettingScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SettingScreen from "./src/screens/SettingScreen";
+import { Provider as ReduxProvider } from "react-redux";
+import { store, persistor } from "./src/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Setting">
-        <Stack.Screen
-          name="Setting"
-          component={SettingScreen}
-          options={{title: 'Setting'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Setting">
+            <Stack.Screen
+              name="Setting"
+              component={SettingScreen}
+              options={{ title: "Setting" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </ReduxProvider>
   );
 }
--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,22 +1,41 @@
 import { View, Text, Switch, StyleSheet } from "react-native";
-
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../redux/store";
+import { toggleDarkMode } from "../redux/settingSlice";
 type Props = {};
 
-const SettingScreen = () => (
-  <View style={styles.container}>
-    <View style={styles.item}>
-      <Text>Dark mode</Text>
-      <Switch />
+const SettingScreen = () => {
+  const isDarkModeEnabled = useSelector(
+    (state: RootState) => state.settingReducer.isDarkModeEnabled
+  );
+  const dispatch = useDispatch();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.item}>
+        <Text>Dark Mode</Text>
+        <Switch
+          value={isDarkModeEnabled}
+          onValueChange={() => {
+            dispatch(toggleDarkMode());
+          }}
+        />
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     width: "100%",
     padding: 16,
   },
-  item: { flexDirection: "row", justifyContent: "space-between", padding: 10, alignItems: "center" },
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    alignItems: "center",
+  },
 });
 
 export default SettingScreen;
